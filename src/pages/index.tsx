5899e85ea6d3e6bd2fc8682b84df61e1a3d44b30
YNGREDH COSTA DA CRUZ <yngredh.costa@gmail.com>
2021-06-16 12:39:22 -0300

    Novos componentes da interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import SafeEnvironment from "../ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "../ui/components/data-display/PageTitle/PageTitle";
 import UserInformation from "../ui/components/data-display/UserInformation/UserInformation";
+import TextFieldMask from "../ui/components/inputs/TextFieldMask/TextFieldMask";
+import { Button, Typography, Container } from "@material-ui/core";
+import {
+  FormElementsContainer,
+  ProfissionaisPaper,
+  ProfissionaisContainer,
+} from "../ui/styles/pages/index.style";
 
 export default function Home() {
   return (
@@ -12,12 +19,65 @@ export default function Home() {
           "Preencha seu endereço e veja todos os profissionais da sua localidade"
         }
       />
-      <UserInformation
-        name={"Yngredh Cruz"}
-        picture={"https://github.com/Yngredh.png"}
-        rating={3}
-        description={"São Paulo"}
-      />
+      <Container>
+        <FormElementsContainer>
+          <TextFieldMask
+            mask={"99.999-999"}
+            label={"Digite seu CEP"}
+            fullWidth
+            variant={"outlined"}
+          />
+          <Typography color={"error"}>CEP inválido</Typography>
+          <Button
+            variant={"contained"}
+            color={"secondary"}
+            sx={{ width: "220px" }}
+          >
+            Buscar
+          </Button>
+        </FormElementsContainer>
+
+        <ProfissionaisPaper>
+          <ProfissionaisContainer>
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+            <UserInformation
+              name={"Yngredh Cruz"}
+              picture={"https://github.com/Yngredh.png"}
+              rating={3}
+              description={"São Paulo"}
+            />
+          </ProfissionaisContainer>
+        </ProfissionaisPaper>
+      </Container>
     </div>
   );
 }
--- src/ui/components/surfaces/Header/Header.style.tsx
+++ src/ui/components/surfaces/Header/Header.style.tsx
@@ -5,13 +5,11 @@ import theme from "../../../themes/theme";
 export const HeaderAppBar = styled(AppBar)`
   background-color: ${({ theme }) => theme.palette.background.paper};
   box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.05);
-
   ${({ theme }) => theme.breakpoints.up("md")} {
     .MuiToolbar-root {
       height: 100px;
     }
   }
-
   ${({ theme }) => theme.breakpoints.down("md")} {
     .MuiToolbar-root {
       display: flex;
@@ -22,7 +20,6 @@ export const HeaderAppBar = styled(AppBar)`
 
 export const HeaderLogo = styled("img")`
   height: 25px;
-
   ${({ theme }) => theme.breakpoints.up("md")} {
     height: 47px;
   }
